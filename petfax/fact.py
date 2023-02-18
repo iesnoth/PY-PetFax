c557dfc635f5f6a1b6f694be35d109d2e13c05f8
Victoria Beasley <iesnoth91@gmail.com>
2023-02-18 12:18:20 -0600

    html side of homework done

--- petfax/fact.py
+++ petfax/fact.py
@@ -2,6 +2,8 @@ from flask import (Blueprint,render_template)
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
+#add a new fact
+#type POST
 @bp.route('/new')
 def add_fact():
     return render_template('facts/new.html')
